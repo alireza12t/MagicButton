d852c7de4f083a93f9c332c220a387827b281988
Alireza Toghyiani <mr.toghyiani@icloud.com>
2020-07-29 16:47:03 +0430

    Initial Commit

---
+++ MagicButton/MagicButton.h
@@ -0,0 +1,18 @@
+//
+//  MagicButton.h
+//  MagicButton
+//
+//  Created by ali on 7/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagicButton.
+FOUNDATION_EXPORT double MagicButtonVersionNumber;
+
+//! Project version string for MagicButton.
+FOUNDATION_EXPORT const unsigned char MagicButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicButton/PublicHeader.h>
+
+
