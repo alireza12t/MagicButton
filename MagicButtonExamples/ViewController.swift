be94e06d4fbc155462045a5d7b169331e099dedb
Alireza Toghyiani <mr.toghyiani@icloud.com>
2020-07-29 16:52:18 +0430

    Initial project setup

---
+++ MagicButtonExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicButtonExamples
+//
+//  Created by ali on 7/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
